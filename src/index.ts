9f64f6362f942eee9f3e7625865cfb2b4ad1bb69
Rubén Conde <ruben.conde@ux-key.com>
2021-11-08 12:09:54 +0100

    feat: :sparkles: LOM Generation
    Signed-off-by: Rubén Conde <ruben.conde@ux-key.com>

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,45 @@
 #!/usr/bin/env node
 
+import { LomCompiler } from "@opentech-ux/lom-compiler";
+import { bold, green, red } from "chalk";
 import clear from "clear";
-import { Command } from "commander";
+import { Command, Option } from "commander";
+import path from "path";
 
 const program = new Command();
 
 clear();
 
+const sourceOption = new Option(
+  "-s, --source [directory or file]",
+  "Select the file from which create the simulation"
+).default(".", "Gets all JSON files in the current directory");
+
+const outputOption = new Option(
+  "-o, --output [directory name]",
+  "Folder name where to save the compilation result files"
+).default("OpenTech-UX LOM Compiler");
+
 program
   .version("0.0.0", "-v, --version", "Output the version number")
   .description("Compiler to generate playable HTML wireframe site from layout description model")
-  .option(
-    "-s, --source [directory or file]",
-    "Select the file from which create the simulation",
-    "./**.json"
-  )
-  .option(
-    "-o, --output [directory name]",
-    "Folder name where to save the compilation result files",
-    "lom-compiler"
-  )
+  .addOption(sourceOption)
+  .addOption(outputOption)
   .parse();
 
-console.log(program.opts());
+const options = program.opts();
+
+const main = () => {
+  try {
+    new LomCompiler().source(options.source).outputDir(options.output).compile();
+    console.log(green("Success!"));
+    console.log("Compilation files created at:");
+    console.log(bold(path.join(process.cwd(), options.output)));
+  } catch (error) {
+    console.log(red(bold("ERROR!")));
+    console.log("Compilation failed with the current message:");
+    console.log(error);
+  }
+};
+
+main();
