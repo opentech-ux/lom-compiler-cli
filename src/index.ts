854fa9e6939338af526d5555b9e1d0766d8e9763
Rubén Conde <ruben.conde@ux-key.com>
2021-11-08 11:33:43 +0100

    build: :construction_worker: Changed buiild configuration
    Signed-off-by: Rubén Conde <ruben.conde@ux-key.com>

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,25 @@
 #!/usr/bin/env node
 
-//import {program} from "commander";
+import clear from "clear";
+import { Command } from "commander";
 
-import chalk from "chalk";
-import {program} from "commander";
-import {clear} from "console";
-
-const figlet = require('figlet');
+const program = new Command();
 
 clear();
-console.log(chalk.red(figlet.textSync('pizza-cli', {horizontalLayout: 'full'})));
 
 program
-    .version('0.0.1')
-    .description("An example CLI for ordering pizza's")
-    .option('-p, --peppers', 'Add peppers')
-    .option('-P, --pineapple', 'Add pineapple')
-    .option('-b, --bbq', 'Add bbq sauce')
-    .option('-c, --cheese <type>', 'Add the specified type of cheese [marble]')
-    .option('-C, --no-cheese', 'You do not want any cheese')
-    .parse();
+  .version("0.0.0", "-v, --version", "Output the version number")
+  .description("Compiler to generate playable HTML wireframe site from layout description model")
+  .option(
+    "-s, --source [directory or file]",
+    "Select the file from which create the simulation",
+    "./**.json"
+  )
+  .option(
+    "-o, --output [directory name]",
+    "Folder name where to save the compilation result files",
+    "lom-compiler"
+  )
+  .parse();
 
-console.log('you ordered a pizza with:');
-console.log("peppers: ", program.getOptionValue("peppers")); // console.log('  - peppers');
-console.log("pineapple: ", program.getOptionValue("pineapple")); // console.log('  - pineapple');
-console.log("bbq: ", program.getOptionValue("bbq")); // console.log('  - bbq');
-console.log("cheese: ", program.getOptionValue("cheese")); // ? 'marble' : program.getOptionValue("no-cheese") || 'no';
-console.log("no-cheese: ", program.getOptionValue("no-cheese")); // ? 'marble' : program.getOptionValue("no-cheese") || 'no';
-//console.log('  - %s cheese', cheese);
+console.log(program.opts());
